0855684134152ed916b03d749cfbabc1bed42394
KEBIR Youcef <kebiryoucef07@gmail.com>
2020-07-19 17:55:42 +0100

    change in routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/index', [
+
+Route::get('/', [
     'uses' => 'contactMessageController@create'
 
 ])->name('index');
